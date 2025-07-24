0311340179f31d108c909c976be1d21dafc48945
Jan Buskens <jan@tickedify.com>
2025-07-24 10:35:04 +0200

    ✨ FEATURE: Elegante z-index oplossing - volledige blur + scherpe clone + klikbaar menu v0.11.76

--- public/app.js
+++ public/app.js
@@ -3763,7 +3763,7 @@ class Taakbeheer {
         clone.style.width = rect.width + 'px';
         clone.style.height = rect.height + 'px';
         clone.style.margin = '0';
-        clone.style.zIndex = '1999'; // Onder menu overlay (2000) maar zichtbaar
+        clone.style.zIndex = '2500'; // Boven blur overlay (2000), onder menu content (3000)
         
         // Maak originele taak transparent
         taakItem.style.opacity = '0.1';
